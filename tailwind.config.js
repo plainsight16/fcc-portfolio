ae0f490ea407a5c1d9b6d0782995e4bed26b42c0
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2021-11-20 00:02:46 +0100

    configured taliwindcss...completed nav and hero section..95% done with projects display section

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  mode:"jit",
+  purge: ["./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        "body":["Poppins"],
+        "headers":["Raleway"]
+      },
+      colors:{
+        "main-white": "#f0f0f0",
+        "main-red": "#be3144",
+        "main-blue": "#45567d",
+        "main-gray": "#303841",
+        "hero-gradient-1": "#3a3d40",
+        "hero-gradient-2": "#181719"
+      },
+      fontSize:{
+        "nav-link":"2.2rem",
+        "hero-header":"6rem",
+        "project-header":"4.2rem",
+        "project-title":"2rem"
+      },
+    },
+   
+   
+    container:{
+      center:true,
+      padding:"2rem"
+    }
+  },
+ 
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
